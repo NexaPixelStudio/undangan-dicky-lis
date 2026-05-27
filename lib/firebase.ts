6fd65291fb95959a9e397cba9cef33f066753f5e
Nexa Pixel <johanessuaktiwa@gmail.com>
2026-05-28 01:28:44 +0700

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'ISI_DARI_FIREBASE',
-  authDomain: 'ISI_DARI_FIREBASE',
-  projectId: 'ISI_DARI_FIREBASE',
-  storageBucket: 'ISI_DARI_FIREBASE',
-  messagingSenderId: 'ISI_DARI_FIREBASE',
-  appId: 'ISI_DARI_FIREBASE',
+  apiKey: 'AIzaSyAiiXwt0b1FKkfLMVEJG5Wd-mSXPQmnXpU',
+  authDomain: 'undangan-dicky-lis-fe63a.firebaseapp.com',
+  projectId: 'undangan-dicky-lis-fe63a',
+  storageBucket: 'undangan-dicky-lis-fe63a.firebasestorage.app',
+  messagingSenderId: '182512964850',
+  appId: '1:182512964850:web:fb9d872c75c98cabc5ac6b',
 };
 
 const app = initializeApp(firebaseConfig);
